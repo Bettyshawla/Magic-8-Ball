a70522ad0732f974a0b8f51953da0717e4791592
BettyShawlagit config --global user.email Betty.tada1@gmail.com <betty.tada1@gmail.com>
2022-10-27 18:02:44 -0400

    js modified

--- script.js
+++ script.js
@@ -9,15 +9,16 @@ const answer = document.querySelector("#answer");
 const errorMsg = document.querySelector("#error")
 addUserInput = function(){
     console.log("hello")
-    question.innerText = userInput.value.toUpperCase();
+    question.innerText =  `The Question you asked is: ${userInput.value.toUpperCase()}`;
     
    
-     let source = ["./images/magic8ball_1.png", "./images/magic8ball_2.png", "./images/magic8ball_3.png", "./images/magic8ball_4.png", "./images/magic8ball_5.png", "./images/magic8ball_6.png", "./images/magic8ball_7.png", "./images/magic8ball_8.png", "./images/magic8ball_9.png", "./images/magic8ball_10.png", "./images/magic8ball_11.png", "./images/magic8ball_12.png", "./images/magic8ball_13.png", "./images/magic8ball_14.png", "./images/magic8ball_15.png", "./images/magic8ball_16.png", "./images/magic8ball_17.png", "./images/magic8ball_18.png", "./images/magic8ball_19.png"]
+     let source = ["./images/magic8ball_1.png", "./images/magic8ball_2.png", "./images/magic8ball_3.png", "./images/magic8ball_4.png", "./images/magic8ball_5.png", "./images/magic8ball_7.png", "./images/magic8ball_8.png", "./images/magic8ball_9.png", "./images/magic8ball_10.png", "./images/magic8ball_11.png", "./images/magic8ball_12.png", "./images/magic8ball_13.png", "./images/magic8ball_14.png", "./images/magic8ball_15.png", "./images/magic8ball_16.png", "./images/magic8ball_17.png", "./images/magic8ball_18.png", "./images/magic8ball_19.png"]
 
 
      if(userInput.value === ''){
         errorMsg.style.display = "Block";
        answer.style.display = "none"
+       question.style.display = 'none'
 
       } else if(userInput.value != "" ) {
         answer.src = source[Math.floor(Math.random()*source.length)];
@@ -25,6 +26,7 @@ addUserInput = function(){
         errorMsg.style.display = "none";
         answer.style.width = '30vw';
         answer.style.height = '60vh';
+        question.style.display = "block"
       } 
         userInput.value = ""
 
